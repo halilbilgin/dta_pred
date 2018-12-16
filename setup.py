05f52e385fddeb905c3ef4e699b55b40e60ae43d
Halil Bilgin <bilginhalil@gmail.com>
2018-12-16 15:36:20 +0300

    Update the dependencies and the version of the package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dta_pred',
-      version='0.5',
+      version='0.6',
       description='A specialized deep learning library for protein drug affinity prediction.',
       author='Halil Bilgin',
       author_email='bilginhalil@gmail.com',
